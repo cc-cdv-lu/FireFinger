31eacc00a2bed5e569a28c11c38e1904f3162c42
Julien Hoffmann <julien.hoffmann@ediff.lu>
2018-10-10 14:09:36 +0200

    style fixes of code

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -9,121 +9,139 @@ import { StyleService } from '../../services/style.service';
 import { SessionService } from '../../services/session.service';
 import { StatisticsService } from '../../services/statistics.service';
 import { UserService } from '../../services/user.service';
-import { ReaderService } from '../../services/reader.service';
 
 import { TranslateService } from '@ngx-translate/core';
 
 enum VIEW {
-  CHAR, WORD, LINE
+  CHAR,
+  WORD,
+  LINE
 }
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'main-view',
   templateUrl: './main-view.component.html',
   styleUrls: ['./main-view.component.scss']
 })
 export class MainViewComponent implements OnInit {
-  constructor(public session: SessionService, private electron: ElectronService, private dialog: MatDialog,
-    public style: StyleService, public stats: StatisticsService, private user: UserService, private router: Router,
-    public translate: TranslateService) {
-    //TODO - LEVEL selection is triggered before login creation
-    let lastLogin = this.electron.config.get("LAST_LOGIN");
-    if (!lastLogin)
+  VIEW = VIEW;
+  // view: VIEW = VIEW.LINE;
+  warningText = '';
+  areSettingsOpen = false;
+  constructor(
+    public session: SessionService,
+    private electron: ElectronService,
+    private dialog: MatDialog,
+    public style: StyleService,
+    public stats: StatisticsService,
+    private user: UserService,
+    private router: Router,
+    public translate: TranslateService
+  ) {
+    // TODO - LEVEL selection is triggered before login creation
+    const lastLogin = this.electron.config.get('LAST_LOGIN');
+    if (!lastLogin) {
       this.router.navigateByUrl('/login');
-    if (lastLogin)
+    }
+    if (lastLogin) {
       this.user.login(lastLogin.name);
+    }
 
     this.session.restoreSession();
-    if (!this.session.isSessionLoaded)
+    if (!this.session.isSessionLoaded) {
       this.router.navigateByUrl('/levels');
+    }
 
     this.view = VIEW.LINE;
 
-    document.documentElement.style.fontSize = '20px'
+    document.documentElement.style.fontSize = '20px';
   }
 
   activateDebug = false;
   @HostListener('window:keydown', ['$event'])
   keyEvent(event: KeyboardEvent) {
-    if (event.ctrlKey) return;
+    if (event.ctrlKey) {
+      return;
+    }
     /*
     if (event.keyCode == 107 && event.ctrlKey) return this.style.increaseFont();
     if (event.keyCode == 109 && event.ctrlKey) return this.style.decreaseFont();
     if (event.keyCode == 68 && event.ctrlKey && event.altKey) return this.activateDebug = !this.activateDebug;
     */
     this.blurAllButtons();
-    if (this.areSettingsOpen) return;
+    if (this.areSettingsOpen) {
+      return;
+    }
     this.session.handleKeyEvent(event);
   }
-  ngOnInit() {
-  }
-
-
-  VIEW = VIEW;
-  //view: VIEW = VIEW.LINE;
-  warningText = "";
-
-  areSettingsOpen: boolean = false;
-
+  ngOnInit() {}
 
   /* Shortcuts */
   getPrev() {
-    return this.session.getPrevSegment(this.view)
+    return this.session.getPrevSegment(this.view);
   }
 
   getNext() {
-    return this.session.getNextSegment(this.view)
-
+    return this.session.getNextSegment(this.view);
   }
 
   getCurrent() {
-    return this.attemptHighlight(this.session.getCurrentChar())
+    return this.attemptHighlight(this.session.getCurrentChar());
   }
 
   getWarningText() {
-    let output = "";
-    if (!this.session) return output;
-    let c: string = this.session.getCurrentChar();
-    if (!c) return output;
+    let output = '';
+    if (!this.session) { return output; }
+    const c: string = this.session.getCurrentChar();
+    if (!c) { return output; }
     switch (c) {
-      case "\n": return this.translate.instant('keys.enter');
-      case " ": return this.translate.instant('keys.space');
-      case "\'": return this.translate.instant('keys.apostrophe');
-      case "ä": case "Ä": case "ü": case "Ü": case "ö": case "Ö": output += this.translate.instant('keys.umlaut')
+      case '\n':
+        return this.translate.instant('keys.enter');
+      case ' ':
+        return this.translate.instant('keys.space');
+      case '\'':
+        return this.translate.instant('keys.apostrophe');
+      case 'ä':
+      case 'Ä':
+      case 'ü':
+      case 'Ü':
+      case 'ö':
+      case 'Ö':
+        output += this.translate.instant('keys.umlaut');
     }
 
     // Check if the letter is uppercase
-    if (c.toLowerCase() != c) {
-      if (output != "") output += "\n";
-      output += this.translate.instant('keys.uppercase')
+    if (c.toLowerCase() !== c) {
+      if (output !== '') { output += '\n'; }
+      output += this.translate.instant('keys.uppercase');
     }
     return output;
   }
 
-
   attemptHighlight(char: string) {
-    if (!char) return;
-    if (char.length > 1) return "";
-    if (char == " ") return "_";
-    if (char == "\n") return "↲";
+    if (!char) { return; }
+    if (char.length > 1) { return ''; }
+    if (char === ' ') { return '_'; }
+    if (char === '\n') { return '↲'; }
     return char;
   }
 
-  setViewMode(VIEW: VIEW) {
-    this.view = VIEW;
+  setViewMode(newVIEW: VIEW) {
+    this.view = newVIEW;
   }
 
   get view() {
-    return this.session.currentChapter.type
+    return this.session.currentChapter.type;
   }
   set view(v: any) {
-    console.log("No longer supported...");
+    console.log('No longer supported...');
   }
 
   openSettings() {
-    if (this.areSettingsOpen) return;
+    if (this.areSettingsOpen) { return; }
     this.areSettingsOpen = true;
-    let dialogRef = this.dialog.open(SettingsComponent, {
+    const dialogRef = this.dialog.open(SettingsComponent, {
       height: '80%',
       width: '55%',
       data: {
@@ -132,15 +150,15 @@ export class MainViewComponent implements OnInit {
     });
     dialogRef.beforeClose().subscribe(() => {
       this.areSettingsOpen = false;
-    })
+    });
   }
 
   /* DEBUG stuff */
 
   blurAllButtons() {
-    let buttons = document.getElementsByTagName('button');
+    const buttons = document.getElementsByTagName('button');
     for (let i = 0; i < buttons.length; i++) {
-      buttons.item(i).blur()
+      buttons.item(i).blur();
     }
   }
 
@@ -151,5 +169,4 @@ export class MainViewComponent implements OnInit {
   skipLetter() {
     this.session.nextTextIndex();
   }
-
 }
--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { ElectronService } from '../providers/electron.service'
+import { ElectronService } from '../providers/electron.service';
 
 import { StatisticsService, Statistics } from './statistics.service';
 import { LessonService, Lesson, Chapter } from './lesson.service';
@@ -13,47 +13,86 @@ import { SoundEffectService, SOUNDS } from './sound-effect.service';
 const LAST_SESSION_KEY = 'LAST_SESSION';
 
 export const enum GAME_STATE {
-  PLAYING, SUCCESS, FAILURE
+  PLAYING,
+  SUCCESS,
+  FAILURE
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
-
-  //TODO add this to settings
-  maxMistakePercentage = 5; //in percentage
-  maxMistakeCount = 50;  // as mistakes per session
-  minTypeSpeed = 1;        //as characters per seconds? TODO revisit stats for this
+  // TODO add this to settings
+  maxMistakePercentage = 5; // in percentage
+  maxMistakeCount = 50; // as mistakes per session
+  minTypeSpeed = 1; // as characters per seconds? TODO revisit stats for this
   gameState = GAME_STATE.PLAYING;
 
   isSessionLoaded = false;
 
   // Session data
   private indexInText = 0;
-  last_wrong_char = "";
+  last_wrong_char = '';
 
-  constructor(private stats: StatisticsService, private lesson: LessonService, private router: Router,
-    private user: UserService, private electron: ElectronService, private sound: SoundEffectService,
-    private stringHelper: StringHelperService, public reader: ReaderService) { }
+  currentLesson: Lesson;
+  currentChapter: Chapter;
+  currentIndex: number;
+
+  constructor(
+    private stats: StatisticsService,
+    private lesson: LessonService,
+    private router: Router,
+    private user: UserService,
+    private electron: ElectronService,
+    private sound: SoundEffectService,
+    private stringHelper: StringHelperService,
+    public reader: ReaderService
+  ) {}
 
   ignoreKeys = [
-    "ContextMenu", "CapsLock", "Tab", "Insert", "Home",
-    "PageUp", "PageDown", "Delete", "End", "Backspace",
-    "Shift", "Control", "Alt", "AltGraph", "Meta", "Dead",
-    "F1", "F2", "F3", "F4", "F5", "F6", "F7",
-    "F8", "F9", "F10", "F11", "F12"
+    'ContextMenu',
+    'CapsLock',
+    'Tab',
+    'Insert',
+    'Home',
+    'PageUp',
+    'PageDown',
+    'Delete',
+    'End',
+    'Backspace',
+    'Shift',
+    'Control',
+    'Alt',
+    'AltGraph',
+    'Meta',
+    'Dead',
+    'F1',
+    'F2',
+    'F3',
+    'F4',
+    'F5',
+    'F6',
+    'F7',
+    'F8',
+    'F9',
+    'F10',
+    'F11',
+    'F12'
   ];
 
   shouldIgnore(key: string): boolean {
     /* If pressed key is part of the list, ignore it (return true)*/
     /* If it is not part of the list (indexOf = -1), true false */
-    if (this.ignoreKeys.indexOf(key) == -1) return false;
+    if (this.ignoreKeys.indexOf(key) === -1) {
+      return false;
+    }
     return true;
   }
 
   handleKeyEvent(event: KeyboardEvent) {
-    if (!this.isSessionLoaded) return console.log("No session loaded...");
+    if (!this.isSessionLoaded) {
+      return console.log('No session loaded...');
+    }
 
     // What should happen when level is over
     if (this.getIndex() >= this.getText().length - 1) {
@@ -61,47 +100,62 @@ export class SessionService {
     }
 
     // Handle special keys
-    let pressedKey = event.key;
-    let expectedKey = this.stringHelper.getCurrentChar(this.getText(), this.getIndex())
-    if (pressedKey == "Escape") return this.reset();
-    if (this.shouldIgnore(pressedKey)) return;
-
-    if (pressedKey == 'ArrowDown')
-      return this.reader.play(this.getCurrentChar(), this.currentChapter.type)// play current char
-    if (pressedKey == 'ArrowRight')
-      return this.reader.play(this.stringHelper.getWordAt(this.getText(), this.getIndex()), 2);//play current word
-    if (pressedKey == 'ArrowLeft') return;
-    if (pressedKey == 'ArrowUp') return;
-
-
-
-    //Default case
-    if (pressedKey == this.getCurrentChar() || pressedKey == 'Pause')
-      return this.nextTextIndex()
-    // Special cases
-    if (pressedKey == 'Enter' && this.getCurrentChar() == '\n')
-      return this.nextTextIndex()
-
-    if (this.getCurrentChar() == '�')
-      return this.nextTextIndex()
-
-    // If wrong letter was entered
-    this.sound.play(SOUNDS.ERROR);
-    if (pressedKey != this.last_wrong_char) {
-      console.log("Entered " + pressedKey + " instead of " + expectedKey);
-      this.last_wrong_char = pressedKey;
-      this.stats.logMistakes(pressedKey, expectedKey)
+    const pressedKey = event.key;
+    const expectedKey = this.stringHelper.getCurrentChar(
+      this.getText(),
+      this.getIndex()
+    );
+    if (pressedKey === 'Escape') {
+      return this.reset();
+    }
+    if (this.shouldIgnore(pressedKey)) {
       return;
     }
 
+    if (pressedKey === 'ArrowDown') {
+      return this.reader.play(this.getCurrentChar(), this.currentChapter.type);
+    } // play current char
+    if (pressedKey === 'ArrowRight') {
+      return this.reader.play(
+        this.stringHelper.getWordAt(this.getText(), this.getIndex()),
+        2
+      );
+    } // play current word
+    if (pressedKey === 'ArrowLeft') {
+      return;
+    }
+    if (pressedKey === 'ArrowUp') {
+      return;
+    }
+
+    // Default case
+    if (pressedKey === this.getCurrentChar() || pressedKey === 'Pause') {
+      return this.nextTextIndex();
+    }
+    // Special cases
+    if (pressedKey === 'Enter' && this.getCurrentChar() === '\n') {
+      return this.nextTextIndex();
+    }
+
+    if (this.getCurrentChar() === '�') {
+      return this.nextTextIndex();
+    }
+
+    // If wrong letter was entered
+    this.sound.play(SOUNDS.ERROR);
+    if (pressedKey !== this.last_wrong_char) {
+      console.log('Entered ' + pressedKey + ' instead of ' + expectedKey);
+      this.last_wrong_char = pressedKey;
+      this.stats.logMistakes(pressedKey, expectedKey);
+      return;
+    }
   }
 
   onEndOfChapter(event) {
     // Log success relevant values
-    let mistakeCount = this.stats.currentStats.mistakesCount;
-    let mistakePercentage = this.stats.currentStats.mistakesCount;
-    let typeSpeed = this.stats.currentStats.typeSpeed;
-
+    const mistakeCount = this.stats.currentStats.mistakesCount;
+    const mistakePercentage = this.stats.currentStats.mistakesCount;
+    const typeSpeed = this.stats.currentStats.typeSpeed;
 
     // Calculating overall stats
     this.user.loggedInUser.lastSessionStats = this.stats.currentStats;
@@ -109,35 +163,39 @@ export class SessionService {
     this.user.recalculateOverallStats();
     this.user.saveUserChanges();
 
-
     // Check if fast enough & didn't make to many mistakes
-    if (mistakeCount <= this.maxMistakeCount &&
+    if (
+      mistakeCount <= this.maxMistakeCount &&
       mistakePercentage <= this.maxMistakePercentage &&
-      typeSpeed >= this.minTypeSpeed) {
+      typeSpeed >= this.minTypeSpeed
+    ) {
       this.onSuccess();
-    }
-
-    else {
-      if (mistakeCount > this.maxMistakeCount) console.log("Too many mistakes");
-      if (mistakePercentage > this.maxMistakePercentage) console.log("Too high mistake percentage!");
-      if (typeSpeed < this.minTypeSpeed) console.log("Too slow!");
+    } else {
+      if (mistakeCount > this.maxMistakeCount) {
+        console.log('Too many mistakes');
+      }
+      if (mistakePercentage > this.maxMistakePercentage) {
+        console.log('Too high mistake percentage!');
+      }
+      if (typeSpeed < this.minTypeSpeed) {
+        console.log('Too slow!');
+      }
       this.onFailure();
     }
-
 
     this.reset();
   }
 
   onSuccess() {
     this.nextChapter();
-    console.log("BIG success! Next level coming up!");
+    console.log('BIG success! Next level coming up!');
     this.sound.play(SOUNDS.SUCCESS);
     this.gameState = GAME_STATE.SUCCESS;
     this.router.navigateByUrl('summary');
   }
 
   onFailure() {
-    console.log("Better luck next time");
+    console.log('Better luck next time');
     this.sound.play(SOUNDS.FAILURE);
     this.gameState = GAME_STATE.FAILURE;
     this.router.navigateByUrl('summary');
@@ -150,12 +208,17 @@ export class SessionService {
   }
 
   getText() {
-    if (!this.currentLesson) return "";
+    if (!this.currentLesson) {
+      return '';
+    }
     if (!this.isSessionLoaded) {
       switch (this.gameState) {
-        case GAME_STATE.SUCCESS: return "✌️";
-        case GAME_STATE.FAILURE: return "👎";
-        default: return "Something went wrong..."
+        case GAME_STATE.SUCCESS:
+          return '✌️';
+        case GAME_STATE.FAILURE:
+          return '👎';
+        default:
+          return 'Something went wrong...';
       }
     }
 
@@ -163,34 +226,41 @@ export class SessionService {
   }
 
   getIndex() {
-    if (!this.indexInText) return 0;
+    if (!this.indexInText) {
+      return 0;
+    }
     return this.indexInText;
   }
 
   getMistakePercentageAsNumber() {
-    if (!this.getText()) return 0;
-    if (this.getText().length < 1) return 0;
-    return Math.round(this.stats.currentStats.mistakesCount / this.getText().length * 100);
-
+    if (!this.getText()) {
+      return 0;
+    }
+    if (this.getText().length < 1) {
+      return 0;
+    }
+    return Math.round(
+      (this.stats.currentStats.mistakesCount / this.getText().length) * 100
+    );
   }
 
   getMistakePercentage() {
-    return this.getMistakePercentageAsNumber() + '%'
+    return this.getMistakePercentageAsNumber() + '%';
   }
 
   saveSession() {
-    let session = {
+    const session = {
       lesson: this.currentLesson,
       chapter: this.currentChapter,
       index: this.currentIndex
-    }
+    };
     this.electron.config.set(LAST_SESSION_KEY, session);
   }
   restoreSession() {
-    let session = this.electron.config.get(LAST_SESSION_KEY);
+    const session = this.electron.config.get(LAST_SESSION_KEY);
     if (!session) {
       this.isSessionLoaded = false;
-      return console.log("No previous session was found...");
+      return console.log('No previous session was found...');
     }
     this.currentLesson = session.lesson;
     this.currentChapter = session.chapter;
@@ -198,9 +268,6 @@ export class SessionService {
     this.isSessionLoaded = true;
   }
 
-  currentLesson: Lesson;
-  currentChapter: Chapter;
-  currentIndex: number;
   loadSession(lesson: Lesson, index: number) {
     this.reset();
     this.currentLesson = lesson;
@@ -211,58 +278,60 @@ export class SessionService {
   }
 
   getPrevSegment(view) {
-    let str = this.getText();
-    let index = this.getIndex();
-    return this.stringHelper.getPrevSegment(str, index, view)
+    const str = this.getText();
+    const index = this.getIndex();
+    return this.stringHelper.getPrevSegment(str, index, view);
   }
 
   getNextSegment(view) {
-    let str = this.getText();
-    let index = this.getIndex();
-    return this.stringHelper.getNextSegment(str, index, view)
+    const str = this.getText();
+    const index = this.getIndex();
+    return this.stringHelper.getNextSegment(str, index, view);
   }
 
   getCurrentChar() {
-    let str = this.getText();
-    let index = this.getIndex();
+    const str = this.getText();
+    const index = this.getIndex();
     return this.stringHelper.getCurrentChar(str, index);
   }
 
   nextTextIndex() {
-    this.last_wrong_char = "";
+    this.last_wrong_char = '';
     this.stats.checkSpeed();
 
-    if (this.getIndex() < this.getText().length)
+    if (this.getIndex() < this.getText().length) {
       this.indexInText++;
-    else
-      return console.log("Cannot continue, reached end of text!");
+    } else {
+      return console.log('Cannot continue, reached end of text!');
+    }
 
-    let word = this.stringHelper.getWordAt(this.getText(), this.getIndex());
+    const word = this.stringHelper.getWordAt(this.getText(), this.getIndex());
 
-    if (word.length > 2 && this.getText()[this.getIndex() - 1] == " ")
-      this.reader.play(word, this.currentChapter.type)
-    else
+    if (word.length > 2 && this.getText()[this.getIndex() - 1] === ' ') {
+      this.reader.play(word, this.currentChapter.type);
+    } else {
       this.reader.play(this.getCurrentChar(), this.currentChapter.type);
+    }
   }
 
-  //index = 0;
+  // index = 0;
   nextChapter() {
-    if (!this.currentLesson) return console.log("No lesson loaded...");
-    if (this.currentIndex < this.currentLesson.chapters.length - 1) {
-      this.loadSession(this.currentLesson, this.currentIndex + 1)
+    if (!this.currentLesson) {
+      return console.log('No lesson loaded...');
     }
-    else {
-      //TODO reached end of chapters
-      console.log("Reached end of lesson!");
+    if (this.currentIndex < this.currentLesson.chapters.length - 1) {
+      this.loadSession(this.currentLesson, this.currentIndex + 1);
+    } else {
+      // TODO reached end of chapters
+      console.log('Reached end of lesson!');
     }
   }
 
   previousChapter() {
     if (this.currentIndex > 0) {
-      this.loadSession(this.currentLesson, this.currentIndex - 1)
-    }
-    else {
-      console.log("Reached beginning of lesson")
+      this.loadSession(this.currentLesson, this.currentIndex - 1);
+    } else {
+      console.log('Reached beginning of lesson');
     }
   }
 }
