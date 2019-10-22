1797e1bafd650257bfcbca768afb9a821dade955
Julien Hoffmann <julien.hoffmann@ediff.lu>
2019-10-22 09:49:53 +0200

    fix for enter button

--- src/app/shared/components/main-view/main-view.component.ts
+++ src/app/shared/components/main-view/main-view.component.ts
@@ -86,8 +86,8 @@ export class MainViewComponent implements OnInit {
   getNext() {
     return this.session.getNextSegment(this.view);
   }
-  getCurrent() {
-    return this.session.getCurrentChar();
+  getCurrentForBraille() {
+    return this.session.getCurrentChar().replace('\n', '\u2880');
   }
 
   getCurrentWithHighlight() {
