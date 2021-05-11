5edcf398a5850402f6113411a41cc88eeb6177c4
Julien Hoffmann <julien.hoffmann@ediff.lu>
2021-05-11 16:53:25 +0200

    added some to dos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,20 @@ import { version } from '../../package.json';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  /*
+  - Text parser
+    * Supported file types? also support docx? What about encoding?
+  - Theme switcher: https://github.com/marcos-dev/ionic-theme-switcher
+  - Stats, error tracking, timer when leaving application mid session?
+  - Save session in progress for that user
+  - Fingers to use etc.
+  - Import old mechanics? Image descriptors
+  - Make the behaviour while typing texts more modular, extendable
+    * To consider: neurovisual disabilities
+    * To consider: multiline texts?
+
+*/
+
   public appPages = [
     { title: 'Home', url: 'home', icon: 'home' },
     { title: 'Chapters', url: 'chapters', icon: 'book' },
