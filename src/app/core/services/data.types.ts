e486791a9646dd04a8f9e90850ffb35005126680
Julien Hoffmann <julien.hoffmann@ediff.lu>
2021-05-14 14:45:24 +0200

    preparations for config architecture

--- src/app/core/services/data.types.ts
+++ src/app/core/services/data.types.ts
@@ -12,9 +12,20 @@ export const DEFAULT_STYLE: Style = {
 };
 
 /* STATS */
-export type Stats = {};
+export type UserStats = {};
+export type LessonStats = {
+  rating: number;
+  time: number;
+  mistakes: number;
+  length: number;
+};
+export type SessionsStats = {
+  // How many letters were typed since last mistake
+  combo: number;
+  sessionStats: LessonStats;
+}
 
-export const DEFUALT_STATS: Stats = {};
+export const DEFUALT_STATS: UserStats = {};
 
 /* USER */
 export const DEFAULT_USER: User = {
@@ -23,8 +34,29 @@ export const DEFAULT_USER: User = {
 };
 
 export type User = {
-  stats: Stats;
+  stats: UserStats;
   name: string;
+  completedLessons?: Array<CompletedLesson>;
 };
 
 /*xxxx*/
+
+/* COURSES */
+export type Course = {
+  lessons: Array<Lesson>;
+  id: string;
+  name: string;
+  description: string;
+};
+export type Lesson = {
+  id: string;
+  name: string;
+  description: string;
+  type: string; // Courses type (text, image, ...)
+  display: string; // Prefered display style
+};
+// Saved in user config?
+export type CompletedLesson = {
+  date: Date;
+  stats: LessonStats;
+};
